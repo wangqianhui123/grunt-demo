604071a26ac96b3e0f40c3153d37e90f0f4d3ac8
wangqianhui123 <1737681410@qq.com>
2020-03-10 10:44:49 +0800

    customize

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,27 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-        sprite: {                               
-          files: {
-            src: ['images/*.gruntpng'],
-            dest: 'dist/bundle.png',
-            destCss:'dist/sprite.css'
-                                       
-          }
-              }
-                
+    htmlmin: {
+      options: {
+        collapseWhitespace: true,
+        preserveLineBreaks: false
+      },
+      files: {
+        src: 'public/index.html',
+        dest: 'dist/index.html'
+      }
+    },
+    uglify: {
+      'dist/bundle.js': 'public/bundle.js'
+    },
+    cssmin: {
+      'dist/layout.css': 'public/layout.css'
+    }
   });
 
-    grunt.loadNpmTasks('grunt-spritesmith');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadTasks('./grunt-task');
 
-      grunt.registerTask('default', ['sprite']);    
-
+  grunt.registerTask('default', ['build', 'cssmin', 'htmlmin', 'uglify']);
 };
-
