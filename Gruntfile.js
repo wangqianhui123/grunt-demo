c121f59268b24fa4d09138682d2bc1209be121dc
wangqianhui123 <1737681410@qq.com>
2020-03-10 08:57:36 +0800

    imagemn

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,19 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    uglify: {
-        files: {
-          'rectangle.min.js': 'rectangle.js'
-        } 
-    }
+        imagemin: {                               
+          files: {
+                    expand: true,
+          src: ['images/*.{png,jpg,gif}'],
+                                 dest: 'dist/'
+                                       
+          }
+              }
+                
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-  grunt.registerTask('default', ['uglify']);
+      grunt.registerTask('default', ['imagemin']);    
+
 };
+
